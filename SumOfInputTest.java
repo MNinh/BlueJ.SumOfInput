0a18e4cba47bbaba722d86770f70b5904ea5659f
mike <mike@zipcodes-mbp-5.lan>
2021-02-25 16:55:29 -0500

    SumOfInput

--- SumOfInputTest.java
+++ SumOfInputTest.java
@@ -8,8 +8,8 @@ import org.junit.Test;
 /**
  * The test class SumOfInputTest.
  *
- * @author  (your name)
- * @version (a version number or a date)
+ * Mike Ninh
+ * 02/25/2021
  */
 public class SumOfInputTest
 {
